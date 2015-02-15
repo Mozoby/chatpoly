c424e7ee6cac52bd2689d306ec63d5613f56c2f6
Mozoby <brthornbury@csupomona.edu>
2015-02-14 23:45:02 -0800

    think I got it finally

--- start_websocket_listener.sh
+++ start_websocket_listener.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export HOME=/home/azureuser
-erl -compile ./chatpoly/client/erlang/websocket.erl
-erl -noshell -s websocket start -s init stop
+exec ./chatpoly/client/erlang/websocket.erl
+# erl -compile ./chatpoly/client/erlang/websocket.erl
+# erl -noshell -s websocket start -s init stop
